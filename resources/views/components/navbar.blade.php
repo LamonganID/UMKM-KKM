d084af17c9d1eb59a18a779282019c651c916cb3
Your Name <you@example.com>
2025-08-03 14:27:31 +0700

    Swab Navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,21 +9,21 @@
         tabindex="0"
         class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li><a href="{{ route('welcome') }}">Home</a></li>
-        <li><a href="{{ route('posts.index') }}">Posts</a></li>
         <li><a href="#profile">Profile</a></li>
         <li><a href="#visiMisi">Visi-Misi</a></li>
+        <li><a href="{{ route('posts.index') }}">Berita</a></li>
         <li><a href="{{ route('albums') }}">Albums</a></li>
         <li><a href="{{ route('contact') }}">Contact</a></li>
       </ul>
     </div>
-    <a class="btn btn-ghost text-xl" href="{{ route('welcome') }}">Lorem</a>
+    <a class="btn btn-ghost text-xl" href="{{ route('welcome') }}">Bandar Kedungmulyo</a>
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal px-1">
       <li><a href="{{ route('welcome') }}">Home</a></li>
-      <li><a href="{{ route('posts.index') }}">Posts</a></li>
       <li><a href="#profile">Profile</a></li>
       <li><a href="#visiMisi">Visi-Misi</a></li>
+      <li><a href="{{ route('posts.index') }}">Berita</a></li>
       <li><a href="{{ route('albums') }}">Albums</a></li>
       <li><a href="{{ route('contact') }}">Contact</a></li>
     </ul>
