ead5149e54cccfbf02a0558f6c89077760fd76ae
Your Name <you@example.com>
2025-08-17 21:36:22 +0700

    `Refactored API routes in api.php, removed unused routes and controllers, and corrected namespace and class names.`

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,14 @@ use Illuminate\Http\Request;
 use Doctrine\DBAL\Schema\Index;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\HomeController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\api\AlbumsController;
+use App\Http\Controllers\api\PhotosController;
+use App\Http\Controllers\Api\PostsController; 
+use App\Http\Controllers\Api\ContactController;
 
 Route::get('/welcome', [HomeController::class,'index']);
-Route::get('/posts', [App\Http\Controllers\Api\PostsController::class, 'index']);
-Route::get('/posts/{slug}', [App\Http\Controllers\Api\PostsController::class, 'show']);
-Route::get('/contact', [App\Http\Controllers\Api\ContactController::class, 'index']);
-Route::get('/albums',[App\Http\Controllers\Api\albumsController::class, 
-'index']);
-Route::get('/photos', [App\Http\Controllers\Api\photosController::class, 'index']);
-Route::get('/albums',[App\Http\Controllers\Api\AlbumsController::class,'index']);
+Route::get('/posts', [PostsController::class, 'index']);
+Route::get('/posts/{slug}', [PostsController::class, 'show']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/albums',[AlbumsController::class, 'index']);
+Route::get('/photos', [PhotosController::class, 'index']);
